02313c686daa761d6926946d590a3d0095da65da
vinivasq <vinivasq@hotmail.com>
2022-08-20 18:04:37 -0300

    challenge update

--- Challenges/script.js
+++ Challenges/script.js
@@ -1,29 +1,46 @@
-let input = document.querySelector('input');
+let input = document.querySelector('#input');
 let texto = document.querySelector('#texto');
 let imagem = document.querySelector('#imagem');
-let letraChave = ['e', 'i', 'a', 'o', 'u'];
 let textoCodificado;
+let textoDecodificado;
 
 texto.style.display = "none";
 
 function criptografar()
 {
-    textoCodificado = input.value;
+    textoCodificado = input.value.toLowerCase();
     
-    for (let i = 0; i < textoCodificado.length; i++) 
-    {    
-
-        if (textoCodificado[i] == 'e') 
-        {
-            textoCodificado[i] = 'enter';
-            console.log(textoCodificado[i]);
-        }
-
-    }
-
-
+    textoCodificado = textoCodificado.replace(/e/g, 'enter');
+    textoCodificado = textoCodificado.replace(/i/g, 'imes');
+    textoCodificado = textoCodificado.replace(/a/g, 'ai');
+    textoCodificado = textoCodificado.replace(/o/g, 'ober');
+    textoCodificado = textoCodificado.replace(/u/g, 'ufat');
+  
     texto.textContent = textoCodificado;
     
     imagem.style.display = "none";
     texto.style.display = "block";
+}
+
+function descriptografar()
+{
+    textoDecodificado = input.value.toLowerCase();
+    
+    textoDecodificado = textoDecodificado.replace(/enter/g, 'e');
+    textoDecodificado = textoDecodificado.replace(/imes/g, 'i');
+    textoDecodificado = textoDecodificado.replace(/ai/g, 'a');
+    textoDecodificado = textoDecodificado.replace(/ober/g, 'o');
+    textoDecodificado = textoDecodificado.replace(/ufat/g, 'u');
+    
+    texto.textContent = textoDecodificado;   
+    
+    imagem.style.display = "none";
+    texto.style.display = "block";
+}
+
+function copiar()
+{
+    texto.select();
+    document.execCommand('copy');
+
 }
