3c5537721783bbc993c86434086625f92e4c106c
vinivasq <vinivasq@hotmail.com>
2022-09-17 16:02:09 -0300

    canvas e forca adicionados e responsivos

--- Challenges/Jogo da forca/script.js
+++ Challenges/Jogo da forca/script.js
@@ -1,6 +1,29 @@
 let tela = document.querySelector("canvas");
 let pincel = tela.getContext('2d');
 
-function DesenhaForca() {
-    
+pincel.strokeStyle = '#0A3871';
+pincel.beginPath();
+pincel.moveTo(300, 10);
+pincel.lineTo(300, 275);
+pincel.stroke();
+pincel.moveTo(250, 275);
+pincel.lineTo(350, 275);
+pincel.stroke();
+
+pincel.moveTo(300, 10);
+pincel.lineTo(400, 10);
+pincel.stroke();
+pincel.lineTo(400, 60);
+pincel.stroke();
+pincel.moveTo(375, 60);
+pincel.lineTo(425, 60);
+pincel.stroke();
+pincel.closePath();
+
+function DesenhaTabuleiro(x) 
+{       
+    pincel.moveTo(x, 350);
+    pincel.lineTo(x + 50, 350);
+    pincel.stroke();
 }
+
