61feae4a1215c7fa3e921a4771d98f718f46a1f5
vinivasq <vinivasq@hotmail.com>
2022-09-22 15:03:23 -0300

    migração das paginas jogo e palavra para o index

--- Challenges/Jogo da forca/script.js
+++ Challenges/Jogo da forca/script.js
@@ -1,5 +1,9 @@
 function IniciaJogo() 
 {
+    BotoesRow();
+    jogo.style.display = 'flex';
+    btnDesistir.style.display = 'flex';
+    btnAdicionaPalavra.style.display = 'none';
     let tela = document.querySelector('canvas');
     let pincel = tela.getContext('2d');
     
@@ -21,7 +25,7 @@ function IniciaJogo()
     pincel.lineTo(425, 60);
     pincel.stroke();
     pincel.closePath();
-    
+
     function DesenhaTabuleiro(x) 
     {       
         pincel.moveTo(x, 350);
@@ -29,19 +33,68 @@ function IniciaJogo()
         pincel.stroke();
     }
     
-    console.log(palavras);
-    var palavraSecreta = Math.ceil(Math.random(palavras) *palavras.length -1);
+    console.log(palavrasChave);
+    var palavraSecreta = Math.ceil(Math.random(palavrasChave) *palavrasChave.length -1);
     
 }
 
-
 function AdicionaPalavra()
 {
+    BotoesRow();
+    btnNovoJogo.style.display = 'none';
+    palavraInput.style.display = 'flex';
+    btnVoltar.style.display = 'flex';
+    btnAdicionaPalavra.style.display = 'flex';
+    btnAdicionaPalavra.classList.replace('btn-secundario', 'btn-primario');
     var input = document.querySelector("textarea");
     palavraAdicionada = input.value
-    palavras.push(palavraAdicionada);
-    console.log(palavras);
+    palavrasChave.push(palavraAdicionada);
+    console.log(palavrasChave);
     input.value = "";
 }
 
-var palavras = ["alura", "carro", "celular", "cachorro", "torta", "espelho", "notebook"]
+function VoltarIndex() {
+    
+    BotoesColumn();
+    jogo.style.display = 'none';
+    palavraInput.style.display = 'none';
+    btnDesistir.style.display = 'none';
+    btnVoltar.style.display = 'none';
+    btnNovoJogo.style.display = 'flex';
+    btnAdicionaPalavra.style.display = 'flex';
+    btnAdicionaPalavra.classList.replace('btn-primario', 'btn-secundario');
+}
+
+function BotoesColumn() {
+    
+    botoes.style.flexDirection = 'column';
+    botoes.style.height= '80vh';
+}
+
+function BotoesRow() {
+    botoes.style.flexDirection = 'row';
+    botoes.style.height= '12vh';
+}
+
+let palavrasChave = ["alura", "carro", "celular", "cachorro", "torta", "espelho", "notebook"];
+
+let jogo = document.getElementById("jogo");
+let palavraInput = document.getElementById("palavra");
+let botoes = document.getElementById("buttons-container");
+let btnNovoJogo = document.getElementById("novo-jogo");
+let btnAdicionaPalavra = document.getElementById("adiciona-palavra");
+let btnDesistir = document.getElementById("desistir");
+let btnVoltar = document.getElementById("voltar");
+
+
+jogo.style.display = 'none';
+palavraInput.style.display = 'none';
+btnDesistir.style.display = 'none';
+btnVoltar.style.display = 'none';
+btnNovoJogo.onclick = IniciaJogo;
+btnAdicionaPalavra.onclick = AdicionaPalavra;
+btnVoltar.onclick = VoltarIndex;
+btnDesistir.onclick = VoltarIndex;
+
+
+
