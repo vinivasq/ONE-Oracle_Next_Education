1d9489dec65cffbf070141f079d5f1c3d3b85540
Vinícius Vasques <vinivasq@hotmail.com>
2022-12-23 18:19:01 -0300

    introdução a java e sua sintaxe

---
+++ Back-end/java/java-jdk-introducao/Programa.java
@@ -0,0 +1,14 @@
+public class Programa {
+    public static void main (String[] args) {
+        System.out.println("Ola mundo");
+
+        int idade;
+        idade = 21;
+
+        System.out.println(idade);
+
+        double salario = 2100.50;
+
+        System.out.println("Meu salário é: R$ " + salario);;
+    }
+}
