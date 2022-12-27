3a107b10991eb51333e40d47f3a2bac596cd05c6
Vinícius Vasques <vinivasq@hotmail.com>
2022-12-27 11:49:16 -0300

    contrutor e atributos/metodos estaticos

--- Back-end/java/bytebank/src/Conta.java
+++ Back-end/java/bytebank/src/Conta.java
@@ -1,9 +1,19 @@
 public class Conta {
 
-	double saldo;
-	int agencia;
-	int numero;
-	Cliente titular;
+	private double saldo;
+	private int agencia;
+	private int numero;
+	private Cliente titular;
+	private static int total = 0;
+
+	public Conta(int agencia, int numero) {
+		Conta.total++;
+		System.out.println("O total de contas é " + Conta.total);
+		this.agencia = agencia;
+		this.numero = numero;
+		this.saldo = 100;
+		System.out.println("Estou criando uma conta" + this.numero);
+	}
 
 	public void deposita(double valor) {
 		this.saldo = this.saldo + valor;
@@ -26,4 +36,45 @@ public class Conta {
 		}
 		return false;
 	}
+
+	public double getSaldo() {
+		return this.saldo;
+	}
+
+	public int getNumero() {
+		return this.numero;
+	}
+
+	public void setNumero(int numero) {
+		if (numero <= 0) {
+			System.out.println("Nao pode valor menor igual a 0");
+			return;
+		}
+		this.numero = numero;
+	}
+
+	public int getAgencia() {
+		return this.agencia;
+	}
+
+	public void setAgencia(int agencia) {
+		if (agencia <= 0) {
+			System.out.println("Nao pode valor menor igual a 0");
+			return;
+		}
+		this.agencia = agencia;
+	}
+
+	public void setTitular(Cliente titular) {
+		this.titular = titular;
+	}
+
+	public Cliente getTitular() {
+		return this.titular;
+	}
+
+	public static int getTotal() {
+		return Conta.total;
+	}
+
 }
--- Back-end/java/bytebank/src/TestaMetodo.java
+++ Back-end/java/bytebank/src/TestaMetodo.java
@@ -1,15 +1,15 @@
 public class TestaMetodo {
 	public static void main(String[] args) {
-		Conta contaDoPaulo = new Conta();
-		contaDoPaulo.saldo = 100;
+		Conta contaDoPaulo = new Conta(6646,969060);
+		contaDoPaulo.deposita(100);
 		contaDoPaulo.deposita(50);
-		System.out.println(contaDoPaulo.saldo);
+		System.out.println(contaDoPaulo.getSaldo());
 
 		boolean conseguiuRetirar = contaDoPaulo.saca(20);
-		System.out.println(contaDoPaulo.saldo);
+		System.out.println(contaDoPaulo.getSaldo());
 		System.out.println(conseguiuRetirar);
 
-		Conta contaDaMarcela = new Conta();
+		Conta contaDaMarcela = new Conta(6644,969063);
 		contaDaMarcela.deposita(1000);
 
 		boolean sucessoTransferencia = contaDaMarcela.transfere(300, contaDoPaulo);
@@ -21,8 +21,8 @@ public class TestaMetodo {
 			System.out.println("faltou dinheiro");
 
 		}
-		System.out.println(contaDaMarcela.saldo);
-		System.out.println(contaDoPaulo.saldo);
+		System.out.println(contaDaMarcela.getSaldo());
+		System.out.println(contaDoPaulo.getSaldo());
 	}
 
 }
