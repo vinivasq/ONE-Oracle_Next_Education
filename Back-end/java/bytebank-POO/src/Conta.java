86916e58934413ac99a1f3d9dbd5932945ae4a6c
Vinícius Vasques <vinivasq@hotmail.com>
2022-12-29 16:36:58 -0300

    mudança de nome do projeto

--- Back-end/java/bytebank-POO/src/Conta.java
+++ Back-end/java/bytebank-POO/src/Conta.java
@@ -11,8 +11,6 @@ public abstract class Conta {
 		//System.out.println("O total de contas é " + Conta.total);
 		this.agencia = agencia;
 		this.numero = numero;
-		//this.saldo = 100;
-		//System.out.println("Estou criando uma conta " + this.numero);
 	}
 
 	public void deposita(double valor) {
