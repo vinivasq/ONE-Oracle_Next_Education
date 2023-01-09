3834ad8c5569db16e61e7e58b9e2a10130a47f9d
Vinícius Vasques <vinivasq@hotmail.com>
2023-01-09 17:21:47 -0300

    conhecendo sets e hashset

---
+++ Back-end/java/gerenciador-de-cursos/src/br/com/alura/TestaAluno.java
@@ -0,0 +1,20 @@
+package br.com.alura;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class TestaAluno {
+    public static void main(String[] args) {
+        Set<String> alunos = new HashSet<>();
+        alunos.add("Rodrigo Turini");
+        alunos.add("Alberto Souza");
+        alunos.add("Nico Steppat");
+        alunos.add("Sergio Lopes");
+        alunos.add("Renan Saggio");
+        alunos.add("Mauricio Aniche");
+
+        for (String aluno : alunos) {
+            System.out.println(aluno);
+        }
+    }
+}
