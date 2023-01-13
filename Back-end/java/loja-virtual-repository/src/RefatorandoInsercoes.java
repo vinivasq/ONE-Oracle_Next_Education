062d2f9524e5088316dec60f29aa13a59be42fde
Vinícius Vasques <vinivasq@hotmail.com>
2023-01-13 18:32:47 -0300

    trabalhando com controle transacional

---
+++ Back-end/java/loja-virtual-repository/src/RefatorandoInsercoes.java
@@ -0,0 +1,44 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class RefatorandoInsercoes {
+    public static void main(String[] args) throws SQLException {
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        Connection con = connectionFactory.recuperarConexao();
+        con.setAutoCommit(false);
+
+        try {
+            PreparedStatement stm = con.prepareStatement("INSERT INTO PRODUTO (nome, descricao) VALUES (?, ?)", Statement.RETURN_GENERATED_KEYS);
+    
+            adicionarItem("Smart TV", "Televisão Samsung", stm);
+            adicionarItem("Iphone", "Iphone 12 mini", stm);
+
+            con.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("ROLLBACK EXECUTADO");
+            con.rollback();
+        }
+    }
+
+    private static void adicionarItem(String nome, String descricao, PreparedStatement stm) throws SQLException {
+        stm.setString(1, nome);
+        stm.setString(2, descricao);
+
+        if(nome.equals("Iphone")){
+            throw new RuntimeException("Não foi possível adicionar o produto.");
+        }
+
+        stm.execute();
+
+        ResultSet result = stm.getGeneratedKeys();
+
+        while(result.next()){
+            Integer id = result.getInt(1);
+            System.out.println("O produto inserido foi: " + id);
+        }
+    }
+}
