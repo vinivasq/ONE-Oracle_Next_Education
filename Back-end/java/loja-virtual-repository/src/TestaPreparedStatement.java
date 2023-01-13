e02c54129d19f54c56e23f891ef5ea2ad9db0b95
Vinícius Vasques <vinivasq@hotmail.com>
2023-01-13 17:45:23 -0300

    utilizando preparedStatement

---
+++ Back-end/java/loja-virtual-repository/src/TestaPreparedStatement.java
@@ -0,0 +1,29 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class TestaPreparedStatement {
+    public static void main(String[] args) throws SQLException {
+        String nome = "Mouse'";
+        String descricao = "Mouse sem fio); delete from produto;";
+
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        Connection con = connectionFactory.recuperarConexao();
+
+        PreparedStatement stm = con.prepareStatement("INSERT INTO PRODUTO (nome, descricao) VALUES (?, ?)", Statement.RETURN_GENERATED_KEYS);
+
+        stm.setString(1, nome);
+        stm.setString(2, descricao);
+
+        stm.execute();
+
+        ResultSet result = stm.getGeneratedKeys();
+
+        while(result.next()){
+            Integer id = result.getInt(1);
+            System.out.println("O produto inserido foi: " + id);
+        }
+    }
+}
--- Back-end/java/loja-virtual-repository/src/TestaRemocao.java
+++ Back-end/java/loja-virtual-repository/src/TestaRemocao.java
@@ -1,14 +1,16 @@
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class TestaRemocao {
     public static void main(String[] args) throws SQLException {
         ConnectionFactory connectionFactory = new ConnectionFactory();
         Connection con = connectionFactory.recuperarConexao();
 
-        Statement stm = con.createStatement();
-        stm.execute("DELETE FROM PRODUTO WHERE ID > 2");
+        PreparedStatement stm = con.prepareStatement("DELETE FROM PRODUTO WHERE ID > ?");
+
+        stm.setInt(1, 2);
+        stm.execute();
 
         Integer linhasModificadas = stm.getUpdateCount();
 
